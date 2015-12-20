0270f78d4d4d63315d767c81f40fdd570dd28d0a
Younes <younes.bouayadine@gmail.com>
2015-12-20 21:49:01 +0100

    MAJ des classes: 	ProgrammationSemaine 	ProgrammationTheatre
    Signed-off-by: Younes <younes.bouayadine@gmail.com>

--- Gestion spectacles/src/gestion/ProgrammationSemaine.java
+++ Gestion spectacles/src/gestion/ProgrammationSemaine.java
@@ -1,5 +1,36 @@
 package gestion;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class ProgrammationSemaine {
 
+	private int semaine;
+	private Map<Film, ProgrammationFilm> listeProgFilm;
+	private Map<PieceTheatre, ProgrammationTheatre> listeProgTheatre;
+
+	public ProgrammationSemaine(int semaine) {
+		this.semaine = semaine;
+		this.listeProgFilm = new HashMap<Film, ProgrammationFilm>();
+		this.listeProgTheatre = new HashMap<PieceTheatre, ProgrammationTheatre>();
+	}
+	
+	public void ajouterFilm(Film f) {
+		ProgrammationFilm pF = new ProgrammationFilm();
+		this.listeProgFilm.put(f, pF);
+	}
+	
+	public boolean ajouterSeanceCinema(Film f, SeanceCinema s) {
+		return this.listeProgFilm.get(f).ajouterSeance(s);
+	}
+	
+	public String consulterSeanceCinema(Film f) {
+		return listeProgFilm.get(f).toString();
+	}
+	
+	public void supprimerSeanceCinema(Film f, SeanceCinema sC) {
+		listeProgFilm.get(f).supprimerSeance(sC);
+	}
+	
 }
--- Gestion spectacles/src/gestion/ProgrammationTheatre.java
+++ Gestion spectacles/src/gestion/ProgrammationTheatre.java
@@ -11,19 +11,19 @@ public class ProgrammationTheatre {
 	public ProgrammationTheatre() {
 		this.listeSeances = new TreeSet<SeanceTheatre>();
 	}
-	
+
 	public boolean ajouterSeance(SeanceTheatre sT) {
 		return this.listeSeances.add(sT);
 	}
-	
-	public void supprimerSeance(SeanceTheatre sT) { 
+
+	public void supprimerSeance(SeanceTheatre sT) {
 		this.listeSeances.remove(sT);
 	}
-	
+
 	public Iterator<SeanceTheatre> getIterator() {
 		return this.listeSeances.iterator();
 	}
-	
+
 	public SeanceTheatre rechercherSeance(int jour) {
 		Iterator<SeanceTheatre> it = listeSeances.iterator();
 		while (it.hasNext()) {
@@ -33,22 +33,22 @@ public class ProgrammationTheatre {
 		}
 		System.out.println("Aucune seance ne correspond aux informations saisies");
 		return null;
-		
 	}
-	
-	public double tauxRemplissage(int jour) { 
+
+	public double tauxRemplissage(int jour) {
 		return rechercherSeance(jour).tauxRemplissage();
 	}
-	
-	public double chiffreAffaire() { 
+
+	public double chiffreAffaire() {
 		Iterator<SeanceTheatre> it = getIterator();
 		double chiffreAffaire = 0;
 		while (it.hasNext()) {
-			chiffreAffaire += it.next().totalVendu() * it.next().getSalle().getTarif();
+			chiffreAffaire += it.next().totalVendu()
+					* it.next().getSalle().getTarif();
 		}
 		return chiffreAffaire;
 	}
-	
+
 	public String toString() {
 		String str = "";
 		for (SeanceTheatre element : listeSeances) {
