f9ac0b61552793ded0f2d71f1dcf0c37449a59b6
Younes <younes.bouayadine@gmail.com>
2015-12-22 18:42:22 +0100

    Mise à jour des classes: 	ProgrammationSemaine 	Seance

--- Gestion spectacles/src/gestion/ProgrammationSemaine.java
+++ Gestion spectacles/src/gestion/ProgrammationSemaine.java
@@ -3,6 +3,9 @@ package gestion;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
+//import java.util.Map.Entry;
+import java.util.Set;
+import java.util.HashSet;
 
 public class ProgrammationSemaine {
 
@@ -15,22 +18,63 @@ public class ProgrammationSemaine {
 		this.listeProgFilm = new HashMap<Film, ProgrammationFilm>();
 		this.listeProgTheatre = new HashMap<PieceTheatre, ProgrammationTheatre>();
 	}
-	
+
 	public void ajouterFilm(Film f) {
 		ProgrammationFilm pF = new ProgrammationFilm();
 		this.listeProgFilm.put(f, pF);
 	}
-	
+
 	public boolean ajouterSeanceCinema(Film f, SeanceCinema s) {
 		return this.listeProgFilm.get(f).ajouterSeance(s);
 	}
-	
-	public String consulterSeanceCinema(Film f) {
-		return listeProgFilm.get(f).toString();
+
+	public ProgrammationFilm consulterSeanceCinema(Film f) {
+		return listeProgFilm.get(f);
 	}
-	
+
 	public void supprimerSeanceCinema(Film f, SeanceCinema sC) {
 		listeProgFilm.get(f).supprimerSeance(sC);
 	}
-	
+
+	public Iterator<Film> films() {
+		Set<Film> film = listeProgFilm.keySet();
+		return film.iterator();
+	}
+
+	// C'est moche et ça fait peur. A ignorer ... Pour l'instant.
+	/*
+	 * public Film trouverFilm(String titre) { Iterator<Entry<Film,
+	 * ProgrammationFilm>> it = listeProgFilm.entrySet().iterator();
+	 * while(it.hasNext()) { Map.Entry<Film, ProgrammationFilm> pair =
+	 * (Map.Entry<Film, ProgrammationFilm>)it.next();
+	 * if(pair.getKey().getTitre().equalsIgnoreCase(titre)) { return
+	 * pair.getKey(); } } System.out.println("Le film n'existe pas."); return
+	 * null; }
+	 */
+
+	public Film trouveFilm(String titre) {
+		Iterator<Film> it = films();
+		while (it.hasNext()) {
+			if (it.next().getTitre().equalsIgnoreCase(titre)) {
+				return it.next();
+			}
+		}
+		System.out.println("Le film n'existe pas.");
+		return null;
+	}
+
+	public Set<Film> consulterFilms() {
+		return listeProgFilm.keySet();
+	}
+
+	public Set<SeanceCinema> consulterSeance(Film f, int jour) {
+		Set<SeanceCinema> lesSeances = new HashSet<SeanceCinema>();
+		Iterator<SeanceCinema> itSeanceCinema = listeProgFilm.get(f).getIterator();
+		while (itSeanceCinema.hasNext()) {
+			SeanceCinema suivant = itSeanceCinema.next();
+			if (suivant.getJour() == jour)
+				lesSeances.add(suivant);
+		}
+		return lesSeances;
+	}
 }
--- Gestion spectacles/src/gestion/Seance.java
+++ Gestion spectacles/src/gestion/Seance.java
@@ -11,9 +11,14 @@ public abstract class Seance implements Comparable<Seance> {
 		this.horaire = horaire;
 		this.nbPlacesVenduesTN = 0;
 	}
-	
-	public int getJour() { return this.jour; }
-	public Heure getHoraire() { return this.horaire; }
+
+	public int getJour() {
+		return this.jour;
+	}
+
+	public Heure getHoraire() {
+		return this.horaire;
+	}
 
 	public abstract int nbPlacesDispo();
 
@@ -24,27 +29,25 @@ public abstract class Seance implements Comparable<Seance> {
 	public void vendrePlacesTN(int nbre) {
 		this.nbPlacesVenduesTN += nbre;
 	}
-	
-	
-	
+
 	public int compareTo(Seance s) {
-		if(this.getJour() > s.getJour()) {
-			if(this.getHoraire().compareTo(s.getHoraire()) < 0) {
+		if (this.getJour() > s.getJour()) {
+			if (this.getHoraire().compareTo(s.getHoraire()) < 0) {
 				return -1;
 			}
 		}
-		if(this.getJour() < s.getJour()) {
+		if (this.getJour() < s.getJour()) {
 			return -1;
 		}
-		if(this.getJour() == s.getJour()) {
-			if (this.getHoraire().compareTo(s.getHoraire()) < 0) 
+		if (this.getJour() == s.getJour()) {
+			if (this.getHoraire().compareTo(s.getHoraire()) < 0)
 				return -1;
-			else if (this.getHoraire().compareTo(s.getHoraire()) > 0) 
+			else if (this.getHoraire().compareTo(s.getHoraire()) > 0)
 				return 1;
 		}
 		return 0;
 	}
-	
+
 	public boolean equals(Seance s) {
 		return ((this.jour == s.jour) && (this.horaire.equals(s.horaire)));
 	}
