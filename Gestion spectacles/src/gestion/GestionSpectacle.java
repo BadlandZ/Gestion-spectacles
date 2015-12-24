b527f9732a954063138edb2e384f5ca26f99df23
Younes <younes.bouayadine@gmail.com>
2015-12-24 23:31:13 +0100

    Création de la classe GestionSpectacle.java
    Signed-off-by: Younes <younes.bouayadine@gmail.com>

---
+++ Gestion spectacles/src/gestion/GestionSpectacle.java
@@ -0,0 +1,11 @@
+package gestion;
+
+public class GestionSpectacle {
+	
+	public static void main(String[] args) {
+		EnsembleSalles salles = new EnsembleSalles();
+		EnsembleSallesTheatre sallesTheatre = new EnsembleSallesTheatre();
+		
+	}
+
+}
